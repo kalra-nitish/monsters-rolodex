97ca9a25b275abf914f57e1824b8ed1b893ede0a
NitishKalra <nitishkalra2618@gmail.com>
2020-05-17 18:05:10 +0530

    Updates

---
+++ precache-manifest.f2f10b2913e27fe4e086d364d84f602a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f14ab7e37b78a0575f80cd0a8f7a12",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5650f8d1da71b681481c",
+    "url": "/monsters-rolodex/static/css/main.39978099.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5650f8d1da71b681481c",
+    "url": "/monsters-rolodex/static/js/main.ed8f1a4d.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
